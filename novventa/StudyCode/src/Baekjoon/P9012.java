0738d2675f692638c0e61b444c95d5ab23874633
novventa <novventa@kakao.com>
2023-02-13 01:17:19 +0900

    괄호 / 9012 / 실버4 / X

--- novventa/StudyCode/src/Baekjoon/P9012.java
+++ novventa/StudyCode/src/Baekjoon/P9012.java
@@ -1,2 +1,40 @@
-package Baekjoon;public class P9012 {
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+
+public class P9012 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<tc; i++) {
+
+            Stack<Character> stack = new Stack<>();
+
+            String ps = br.readLine();
+
+            for (int j = 0; j < ps.length(); j++) {
+                char c = ps.charAt(i);
+
+                if (c == '(') {
+                    stack.push(c);
+                }else if (stack.isEmpty()) {
+                    System.out.println("NO");
+                    break;
+                } else {
+                    stack.pop();
+                }
+            }
+            if(stack.isEmpty())
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
 }
