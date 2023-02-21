a96eb5cf31e8340f9b3953ce9ade5faa42f16e65
j-byun <jhye.byun@gmail.com>
2023-02-21 22:48:39 +0900

    색종이 / 2563 / 실버5 / O

--- j-byun/ClassCode/BOJ/BOJ_2563_색종이_변지혜.java
+++ j-byun/ClassCode/BOJ/BOJ_2563_색종이_변지혜.java
@@ -25,60 +25,56 @@ import java.util.StringTokenizer;
 //		얘를 start row로 활용해도 도화지 위아래 뒤집으면 똑같으니까 상관없다
 //		도화지, 색종이 다 정사각형이니까!
 
-
 public class BOJ_2563_색종이_변지혜 {
-	
+
 	static boolean[][] paper = new boolean[101][101]; // 도화지 공간
 	static int blackArea = 0; // 검은 색종이 면적 저장할 공간
-	
-    public static void main(String[] args) throws NumberFormatException, IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringBuilder sb = new StringBuilder();
-    	
-    	int confettiCnt = Integer.parseInt(br.readLine()); // 색종이 개수 입력받기
-    	
-    	for (int cnt = 1; cnt <= confettiCnt; cnt++) { // 색종이 개수만큼 반복
-    		
-    		StringTokenizer st = new StringTokenizer(br.readLine()); // 한 줄 읽어와서 띄어쓰기 단위로 끊기
-    		
-    		int blackCol = Integer.parseInt(st.nextToken()); // 검은색 색종이의 시작 col 입력받기
-    		int blackRow = Integer.parseInt(st.nextToken()); // 검은색 색종이의 끝 row 입력받기
-    		
-    		
-    		confettiOnPaper(blackRow, blackCol); // 도화지에 검은색 색종이 붙이기
-    	}
-    	
-    	blackOnPaperCnt(); // 도화지에 붙은 검은색 색종이 영역 구하기
-    	
-    	sb.append(blackArea);
-    	System.out.println(sb); // 출력
-    	
-    }
-    
-    private static void confettiOnPaper(int blackRow, int blackCol) { // 도화지에 검은색 색종이를 붙이는 method
-    	
-    	for (int row = blackRow; row < blackRow + 10; row++) { // 입력된 row,col 기준으로 +10만큼씩 영역에 검은색 색종이 붙이기
-    		for (int col = blackCol; col < blackCol + 10; col++) {
-    			if (!paper[row][col]) { // 1*1 영역의 현재 칸이 하얀색 도화지 일 때만
-    				paper[row][col] = true; // 검은색 색종이를 붙여준다
-    			}
-    		}
-    	}
-    	
-    }
-    
-    private static void blackOnPaperCnt() { // 도화지에 붙은 검은색 색종이 영역을 구하는 method
-    	
-    	for (int row = 1; row < 101; row++) { // 도화지의 모든 영역을 확인하면서
-    		for (int col = 1; col < 101; col++) {
-    			if (paper[row][col]) { // 1*1 영역이 true(검은색)이면
-    				blackArea++; // 검은색 영역++
-    			}
-    		}
-    	}
-    	
-    }
-    
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int confettiCnt = Integer.parseInt(br.readLine()); // 색종이 개수 입력받기
+
+		for (int cnt = 1; cnt <= confettiCnt; cnt++) { // 색종이 개수만큼 반복
+
+			StringTokenizer st = new StringTokenizer(br.readLine()); // 한 줄 읽어와서 띄어쓰기 단위로 끊기
+
+			int blackCol = Integer.parseInt(st.nextToken()); // 검은색 색종이의 시작 col 입력받기
+			int blackRow = Integer.parseInt(st.nextToken()); // 검은색 색종이의 끝 row 입력받기
+
+			confettiOnPaper(blackRow, blackCol); // 도화지에 검은색 색종이 붙이기
+		}
+
+		blackOnPaperCnt(); // 도화지에 붙은 검은색 색종이 영역 구하기
+
+		sb.append(blackArea);
+		System.out.println(sb); // 출력
+
+	}
+
+	private static void confettiOnPaper(int blackRow, int blackCol) { // 도화지에 검은색 색종이를 붙이는 method
+
+		for (int row = blackRow; row < blackRow + 10; row++) { // 입력된 row,col 기준으로 +10만큼씩 영역에 검은색 색종이 붙이기
+			for (int col = blackCol; col < blackCol + 10; col++) {
+				if (!paper[row][col]) { // 1*1 영역의 현재 칸이 하얀색 도화지 일 때만
+					paper[row][col] = true; // 검은색 색종이를 붙여준다
+				}
+			}
+		}
+
+	}
+
+	private static void blackOnPaperCnt() { // 도화지에 붙은 검은색 색종이 영역을 구하는 method
+
+		for (int row = 1; row < 101; row++) { // 도화지의 모든 영역을 확인하면서
+			for (int col = 1; col < 101; col++) {
+				if (paper[row][col]) { // 1*1 영역이 true(검은색)이면
+					blackArea++; // 검은색 영역++
+				}
+			}
+		}
+
+	}
+
 }
-
-
