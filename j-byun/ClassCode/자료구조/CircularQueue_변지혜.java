a606a15ad7f0cabc940a78cbfe201d6b2cb94edf
변지혜 <jhye.byun@gmail.com>
2023-02-23 11:48:49 +0900

    자료구조 / 원형 큐 구현

--- j-byun/ClassCode/자료구조/CircularQueue_변지혜.java
+++ j-byun/ClassCode/자료구조/CircularQueue_변지혜.java
@@ -1,19 +1,19 @@
 
 // 원형 큐
-public class CircularQueue_변지혜 {
-	public static int size = Integer.MAX_VALUE;
-	public static int[] arr = new int[size];
-	public static int front = 0, rear = 0;
+public class Queue {
+	int size = Integer.MAX_VALUE;
+	int[] arr = new int[size];
+	int front = 0, rear = 0;
 
-	public static boolean isEmpty() {
+	public boolean isEmpty() {
 		return front == rear;
 	}
 
-	public static boolean isFull() {
+	public boolean isFull() {
 		return (rear + 1) % size == front;
 	}
 
-	public static void enQueue(int item) {
+	public void enQueue(int item) {
 		if (isFull()) {
 			System.out.println("큐가 가득차서 더 이상 입력할 수 없습니다.");
 			return;
@@ -23,7 +23,7 @@ public class CircularQueue_변지혜 {
 		arr[rear] = item;
 	}
 
-	public static int deQueue() {
+	public int deQueue() {
 		if (isEmpty()) {
 			System.out.println("큐가 비어있어 뺄 수 있는 값이 없습니다");
 			return Integer.MIN_VALUE;
@@ -33,7 +33,7 @@ public class CircularQueue_변지혜 {
 		return arr[front];
 	}
 
-	public static int peek() {
+	public int peek() {
 		if (isEmpty()) {
 			System.out.println("큐가 비어있어 조회할 수  있는 값이 없습니다");
 			return Integer.MIN_VALUE;
@@ -42,7 +42,7 @@ public class CircularQueue_변지혜 {
 		return arr[(front + 1) % size];
 	}
 	
-	public static int size() {
+	public int size() {
 		if (rear >= front) {
 			return rear - front;
 		} else {
@@ -50,7 +50,7 @@ public class CircularQueue_변지혜 {
 		}
 	}
 	
-	public static void print() {
+	public void print() {
 		if (isEmpty()) {
 			System.out.println("큐가 비어있어 출력할 수  있는 값이 없습니다");
 			return;
