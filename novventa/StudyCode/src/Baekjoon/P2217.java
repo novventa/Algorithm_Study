70c3777f2769e73781f7bfd9587c7a82178aef32
novventa <novventa@kakao.com>
2023-08-23 15:47:40 +0900

    로프 / 2217 / 실버4 / O

---
+++ novventa/StudyCode/src/Baekjoon/P2217.java
@@ -0,0 +1,30 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P2217 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] rope = new int[N];
+
+        for(int i=0;i<N;i++){
+            rope[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(rope);
+
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0;i<N;i++){
+            max = Math.max(max, rope[i]*(N-i));
+        }
+
+        System.out.println(max);
+
+    }
+}
