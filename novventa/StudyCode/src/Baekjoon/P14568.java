3b291f6ce93cd1da8fd2eb6101d8d1418e6f4859
novventa <novventa@kakao.com>
2023-12-08 20:16:50 +0900

    2017 연세대학교 프로그래밍 경시대회 / 14568 / 브론즈3 / O

---
+++ novventa/StudyCode/src/Baekjoon/P14568.java
@@ -0,0 +1,20 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P14568 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int s = 0;
+        for (int i = 2; i < n - 1; i += 2) {
+            s += (n - i - 2) / 2;
+        }
+        System.out.println(s);
+
+    }
+}
