8dbcc7d4217fc5e5e5675edb2682e361342d3501
novventa <novventa@kakao.com>
2023-12-12 18:40:36 +0900

    방 배정하기 / 14697 / 브론즈2 / O

---
+++ novventa/StudyCode/src/Baekjoon/P14697.java
@@ -0,0 +1,32 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P14697 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        boolean isPossible = false;
+
+        for (int i = 0; i <= 50; i++) {
+            for (int j = 0; j <= 50; j++) {
+                for (int k = 0; k <= 50; k++) {
+                    if (a * i + j * b + c * k == n)
+                        isPossible = true;
+                }
+            }
+        }
+
+        System.out.println(isPossible ? 1 : 0);
+
+    }
+}
